c2394e23dd53e628a98c4108281f28289583691d
Lauren Bart <lmb54692@gmail.com>
2018-04-05 00:00:29 +0000

    init commit

---
+++ hello.rb
@@ -0,0 +1,8 @@
+puts "hello world"
+puts "what's up friend"
+
+gift = ["puppy", "cat", "treats", "gift card", "letter"]
+
+puts "All I got for Valentine's Day was #{gift.sample}."
+
+puts "my new branch"
